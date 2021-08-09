5328da35ff01019a03c3a4071c86d8f6e53ff16e
dturnip <alexander.xie007@gmail.com>
2021-08-09 21:37:34 +1000

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/app/app.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 id={"strate"}>Hello Strate!</h1>
+    </div>
+  );
+}
+
+export default App;
