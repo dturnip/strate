530a853556019ac10a78927c7ac5f6d6019b2328
dturnip <alexander.xie007@gmail.com>
2021-08-17 09:58:25 +1000

    feat: add boilerplate routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './styles/app/app.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Tutorial from './pages/Tutorial';
 
 function App() {
   return (
-    <div className="App">
-        <h1 id={"strate"}>Hello Strate!</h1>
-    </div>
+      <Router>
+              <Switch>
+                  <Route exact path={"/strate/"}>
+                      <Home />
+                  </Route>
+                  <Route path={"/strate/tutorial"}>
+                      <Tutorial />
+                  </Route>
+              </Switch>
+      </Router>
   );
 }
 
