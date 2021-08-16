a61c0f14f565d3f977dc11fdc7cab337dcb403ec
dturnip <alexander.xie007@gmail.com>
2021-08-17 09:57:49 +1000

    feat: add boilerplate page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BgParticles} from "../components/BgParticles";
+import {Start} from "../components/Start";
+
+const Home = () => {
+    return (
+        <>
+            <BgParticles />
+            <Start />
+        </>
+    )
+}
+
+export default Home;
