4faccf60bd587c9fdb449d4a3381701f267dfad1
dturnip <alexander.xie007@gmail.com>
2021-08-17 13:06:38 +1000

    fix: gh pages react routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,29 @@ import Home from './pages/Home';
 import Tutorial from './pages/Tutorial';
 
 function App() {
-  return (
-      <Router>
-              <Switch>
-                  <Route exact path={"/strate/"}>
-                      <Home />
-                  </Route>
-                  <Route path={"/strate/tutorial"}>
-                      <Tutorial />
-                  </Route>
-              </Switch>
-      </Router>
-  );
+    console.log(process.env.PUBLIC_URL);
+    return (
+        <Router basename={process.env.PUBLIC_URL}>
+            <Switch>
+                <Route exact path={"/"} component={Home} />
+                <Route exact path={"/tutorial"} component={Tutorial} />
+            </Switch>
+        </Router>
+    )
+  // return (
+  //     <Router basename={process.env.PUBLIC_URL}>
+  //             <Switch>
+  //                 <Route exact path={"/strate/"}>
+  //                     <Home />
+  //                 </Route>
+  //                 <Route path={"/strate/tutorial"}>
+  //                     <Tutorial />
+  //                 </Route>
+  //                 <Route exact path={"/"} component={Home} />
+  //                 <Route exact path={"/tutorial"} component={Tutorial} />
+  //             </Switch>
+  //     </Router>
+  // );
 }
 
 export default App;
