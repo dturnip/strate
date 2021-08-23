deb86a5f107bab36ce8d820b6ea5acb3ae022159
dturnip <alexander.xie007@gmail.com>
2021-08-24 04:41:27 +1000

    feat: add GameBoard component to page

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { GameBoard } from "../components/GameBoard";
 
 const Tutorial = () => {
     return (
         <>
             <h1>Tutorial</h1>
+            <GameBoard />
         </>
     )
 }
