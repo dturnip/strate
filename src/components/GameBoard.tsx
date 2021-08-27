13dd4a86bf0a64037752862982421f7838622728
dturnip <alexander.xie007@gmail.com>
2021-08-27 18:18:07 +1000

    test: gitkraken op

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -6,18 +6,21 @@ import { morphLeft, morphRight, morphUp, morphDown } from "../lib/GridLogic";
 
 const Cell: React.FC<GameCell> = ({n}) => {
     return (
-        <div className={"cell"}>
-            {n}
+        <div className={`cell type-${n}`}>
+            {n > 0 ? n : ""}
         </div>
     );
 }
 
 // replace with custom map later
-let grid: Array<Array<number>> = [];
-for (let r = 0; r < gridN; r++) {
-    grid.push([]);
-    for (let c = 0; c < gridN; c++) {
-        grid[r].push(1);
+// let grid: Array<Array<number>> = [];
+// for (let r = 0; r < gridN; r++) {
+//     grid.push([]);
+//     for (let c = 0; c < gridN; c++) {
+//         grid[r].push(1);
+//     }
+// }
+
     }
 }
 
