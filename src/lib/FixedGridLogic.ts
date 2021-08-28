b33335f791ceb888a908cbeec7fd26bcedcc7851
dturnip <alexander.xie007@gmail.com>
2021-08-28 21:39:15 +1000

    feat: start fix grid logic

---
+++ src/lib/FixedGridLogic.ts
@@ -0,0 +1,19 @@
+import {NNA} from "../types/aliases";
+
+// shoot
+// export function printMatrix(matrix: NNA) {
+// 	let ret = "";
+// 	for (let r = 0; r < matrix.length; r++) {
+// 		for (let c = 0; c < matrix[r].length; c++) {
+// 			const curr = matrix[r][c].toString();
+// 			if (c < matrix[r].length - 1) {
+// 				ret = `${ret} ${curr}`;
+// 			}
+// 			if (c === matrix[r].length - 1) {
+// 				ret = `${ret} ${curr}\n`;
+// 			}
+// 		}
+// 	}
+// 	return ret;
+// }
+
