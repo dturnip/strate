acdc2587d8337ec2922bcaeb05ffb3558d134737
dturnip <alexander.xie007@gmail.com>
2021-08-28 22:55:52 +1000

    feat: add matrix column/row transformation helper functions

--- src/lib/FixedGridLogic.ts
+++ src/lib/FixedGridLogic.ts
@@ -1,22 +1,5 @@
 import {NNA} from "../types/aliases";
 
-// shoot
-// export function printMatrix(matrix: NNA) {
-// 	let ret = "";
-// 	for (let r = 0; r < matrix.length; r++) {
-// 		for (let c = 0; c < matrix[r].length; c++) {
-// 			const curr = matrix[r][c].toString();
-// 			if (c < matrix[r].length - 1) {
-// 				ret = `${ret} ${curr}`;
-// 			}
-// 			if (c === matrix[r].length - 1) {
-// 				ret = `${ret} ${curr}\n`;
-// 			}
-// 		}
-// 	}
-// 	return ret;
-// }
-
 export const compressRight = (matrix: NNA): NNA => {
 	let ret: NNA = [];
 	for (let r = 0; r < matrix.length; r++) {
@@ -36,6 +19,22 @@ export const compressLeft = (matrix: NNA): NNA => {
 	}
 
 	return ret;
+}
+
+export const transposeToColumns = (matrix: NNA): NNA => {
+	let cols: NNA = [];
+	for (let r = 0; r < matrix.length; r++) {
+		let col: Array<number> = [];
+		for (let c = 0; c < matrix.length; c++) {
+			col.push(matrix[c][r]);
+		}
+		cols.push(col);
+	}
+	return cols;
+}
+
+export const transposeToRows = (matrix: NNA): NNA => {
+	return transposeToColumns(matrix);
 }
 
 export const mergeX = (matrix: NNA): NNA => {
