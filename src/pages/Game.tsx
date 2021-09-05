f451af3de58d7ce3454139e5bcbade7294c46e16
dturnip <alexander.xie007@gmail.com>
2021-09-06 08:49:44 +1000

    refactor: drop old code

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -5,21 +5,14 @@ import {getMap} from "../lib/Maps";
 import "../styles/game/game.scss"
 
 const Tutorial = () => {
-	const [level, setLevel] = useState<number>(0);
+    const [level, setLevel] = useState<number>(0);
 
-	const incrementLevel = () => {
-		setLevel(prev => prev + 1);
-		console.log(level);
-	};
+    const incrementLevel = () => {
+        setLevel(prev => prev + 1);
+        console.log(level);
+    };
 
     return (
-        <>
-            <h1 style={{textAlign: 'center'}}>Game</h1>
-            <GameBoard map={getMap(level)} />
-			{/*<GameBoard map={getMap(1)} />*/}
-			<button onClick={incrementLevel}>Incremenet Level!</button>
-        </>
-    )
 }
 
 export default Tutorial;
