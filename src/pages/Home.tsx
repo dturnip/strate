e7f9505705fc2f83742b79eff1141573c481dacc
dturnip <alexander.xie007@gmail.com>
2021-09-06 08:45:09 +1000

    feat: set "coffees" to 0 in localstorage onload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {BgParticles} from "../components/BgParticles";
 import {Start} from "../components/Start";
 
 const Home = () => {
+    const set = (k: string) => (v: any) => localStorage.setItem(k, v);
+    const get = (k: string) => localStorage.getItem(k);
+
+    useEffect(() => {
+        set("coffees")(0);
+    }, []);
+
     return (
         <>
             <BgParticles />
