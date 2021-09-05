722e864dd24a0e11a49066f688bb9372e170315e
dturnip <alexander.xie007@gmail.com>
2021-09-06 08:49:28 +1000

    feat: add imports

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { GameBoard } from "../components/GameBoard";
-import { getMap } from "../lib/Maps";
+import React, {useState, useEffect} from 'react';
+import {GameBoard} from "../components/GameBoard";
+import {Side} from "../components/Side";
+import {getMap} from "../lib/Maps";
+import "../styles/game/game.scss"
 
 const Tutorial = () => {
 	const [level, setLevel] = useState<number>(0);
