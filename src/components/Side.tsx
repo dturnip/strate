8e46f21950bbe097e01e75d815d10bd864d0c727
dturnip <alexander.xie007@gmail.com>
2021-09-06 08:47:36 +1000

    feat: start add side component

---
+++ src/components/Side.tsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+import {SideButton} from "./SideButton";
+
+interface SideProps {
+    points: string;
+    objective: string;
+    moves: string;
+}
+
