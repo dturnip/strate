2ea820741c8c76bf8ca2f079dd9b482f4feecbf9
dturnip <alexander.xie007@gmail.com>
2021-09-06 08:48:09 +1000

    feat: add side button components to side component

--- src/components/Side.tsx
+++ src/components/Side.tsx
@@ -7,3 +7,28 @@ interface SideProps {
     moves: string;
 }
 
+export const Side: React.FC<SideProps> = ({points, objective, moves}) => {
+    return (
+        <>
+            <div id={"side-wrapper"}>
+                {/*<h1>Side</h1>*/}
+                <section id={"points"}>
+                    <SideButton construct={"Points"} data={points}/>
+                </section>
+
+                <section id={"objective"}>
+                    <SideButton construct={"Objective"} data={objective}/>
+                </section>
+
+                <section id={"Moves"}>
+                    <SideButton construct={"Moves"} data={moves}/>
+                </section>
+
+                {/* Experimental Stuff */}
+                {/*<section id={"Completed"}>*/}
+                {/*    <SideButton construct={"[Experimental] Percentage"} data={"0%"}/>*/}
+                {/*</section>*/}
+            </div>
+        </>
+    )
+}
