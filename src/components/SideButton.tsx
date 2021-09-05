a680f4293b46dc206298aea67f619a9b40332abe
dturnip <alexander.xie007@gmail.com>
2021-09-06 08:46:28 +1000

    feat: end add start button component

--- src/components/SideButton.tsx
+++ src/components/SideButton.tsx
@@ -5,3 +5,15 @@ interface SideButtonProps {
     data: string;
 }
 
+export const SideButton: React.FC<SideButtonProps> = ({ construct, data }) => {
+    return (
+        <>
+            <div className={"side-button"}>
+                <span id={"construct"}><u>{construct}</u></span>
+                <br/>
+                <br/>
+                <span id={"data"}>{data}</span>
+            </div>
+        </>
+    )
+}
