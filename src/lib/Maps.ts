60cea99c95c4c32422350a771ea388490efbc6dd
dturnip <alexander.xie007@gmail.com>
2021-09-07 20:48:23 +1000

    dx: add helper function to retrieve map metadata

--- src/lib/Maps.ts
+++ src/lib/Maps.ts
@@ -1,13 +1,18 @@
 import { NNA } from "../types/aliases";
 import maps from "../data/maps.json";
-import { Maps } from "../types/interfaces";
+import meta from "../data/meta.json";
+import { Maps, Metas, Meta } from "../types/interfaces";
 
 const retErrMap = (): NNA => {
 	return [...Array(8)].map(_ => Array(8).fill(2));
 }
 
-export const getAllMaps = (): Maps | null => {
+const getAllMaps = (): Maps | null => {
 	return maps;
+}
+
+const getAllMeta = (): Metas | null => {
+	return meta;
 }
 
 export const getMap = (map: number): NNA => {
@@ -16,3 +21,8 @@ export const getMap = (map: number): NNA => {
 	return maps[map];
 }
 
+export const getMeta: ((map: number) => Meta) = (map) => {
+    const meta = getAllMeta();
+    if (meta === null) return { objective: "Error", moves: 0 };
+    return meta[map];
+}
