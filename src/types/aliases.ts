7adb967b7538fa6c166db1b9c5592c0ac75e1e96
dturnip <alexander.xie007@gmail.com>
2021-09-08 21:01:15 +1000

    feat: add Status enum for game control and two function signatures

--- src/types/aliases.ts
+++ src/types/aliases.ts
@@ -1,2 +1,12 @@
 export type NNA = Array<Array<number>>;
 export type morph = (matrix: NNA) => NNA;
+
+export type CheckFn = (moves: number, cond: boolean) => Status;
+export type StatusFn = (level: number, moves: number) => Status;
+
+export enum Status {
+    CLEAR = "CLEAR",
+    FAIL = "FAIL",
+    ALIVE = "ALIVE",
+    ERROR = "ERROR",
+}
