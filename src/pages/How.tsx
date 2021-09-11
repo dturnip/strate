624a38119f0383e41e383434b6fcfe44b58999c5
dturnip <alexander.xie007@gmail.com>
2021-09-11 21:02:08 +1000

    feat: add control section

--- src/pages/How.tsx
+++ src/pages/How.tsx
@@ -5,6 +5,17 @@ const How = () => {
     return (
         <>
             <div id={"center-flex"}>
+                <div id={"master-flex-wrap"}>
+                    <section id={"control-sect"} className={"sect"}>
+                        <div id={"control-img-wrapper"} className={"img-wrap"}>
+                            <img src={`${process.env.PUBLIC_URL}/assets/how/gridHEIGHT500.png`} alt={"control"} id={"how-control"} className={"img"}/>
+                        </div>
+                        <div id={"control-text-wrapper"} className={"text-wrap"}>
+                            <span id={"control-head"} className={"head"}>Controls</span>
+                            <span id={"control-body"} className={"body"}>Game mechanics are familiar and inspired by 2048. Only identical tiles can merge. Arrow keys are used to move the tiles.</span>
+                        </div>
+                    </section>
+                </div>
             </div>
         </>
     )
