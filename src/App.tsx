d9f8e7d711ddee578f4a1885848b84cf4530a9ad
dturnip <alexander.xie007@gmail.com>
2021-09-11 21:00:08 +1000

    feat: add How screen to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './styles/app/app.scss';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import How from './pages/How';
 import Game from './pages/Game';
 
 function App() {
-    console.log(process.env.PUBLIC_URL);
     return (
         <HashRouter basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route exact path={"/"} component={Home} />
+                <Route exact path={"/how"} component={How} />
                 <Route exact path={"/play"} component={Game} />
             </Switch>
         </HashRouter>
