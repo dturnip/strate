ec22945dce3f67d05aaefd8972717bd76189421d
dturnip <alexander.xie007@gmail.com>
2021-09-11 21:02:15 +1000

    feat: add flow section

--- src/pages/How.tsx
+++ src/pages/How.tsx
@@ -15,6 +15,16 @@ const How = () => {
                             <span id={"control-body"} className={"body"}>Game mechanics are familiar and inspired by 2048. Only identical tiles can merge. Arrow keys are used to move the tiles.</span>
                         </div>
                     </section>
+                    <hr />
+                    <section id={"flow-sect"} className={"sect"}>
+                        <div id={"flow-text-wrapper"} className={"text-wrap"}>
+                            <span id={"flow-head"} className={"head"}>Game Flow</span>
+                            <span id={"flow-body"} className={"body"}>When two identical tiles become merged, the new merged tile gets added to the points score.<br /><br />Each level has an objective which must be cleared within a certain amount of moves in order to move on to the next.</span>
+                        </div>
+                        <div id={"flow-img-wrapper"} className={"img-wrap"}>
+                            <img src={`${process.env.PUBLIC_URL}/assets/how/pointsHEIGHT500.png`} alt={"flow"} id={"how-flow"} className={"img"}/>
+                        </div>
+                    </section>
                 </div>
             </div>
         </>
