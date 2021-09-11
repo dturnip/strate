86a14b250f3009e82a47717065e4407de08d0ac5
dturnip <alexander.xie007@gmail.com>
2021-09-11 21:00:47 +1000

    feat: add button routing to How screen

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -10,16 +10,29 @@ export const Start: React.FC = () => {
                 <div id={"cb-wrapper"}>
                     <img id={"coffee-bk"} src={coffeebk} alt={"coffee-bk"}/>
                 </div>
-                <div id={"bt-wrapper"}>
-                    <Route render={({history}) => (
-                        <span
-                            id={"start-bt"}
-                            onClick={() => {
-                                history.push("/play")
-                            }}>
-                            Start!
+                <div id={"bts-wrapper"}>
+                    <div id={"start-bt-wrapper"}>
+                        <Route render={({history}) => (
+                            <span
+                                id={"start-bt"}
+                                onClick={() => {
+                                    history.push("/play")
+                                }}>
+                            Start Game!
                         </span>
-                    )} />
+                        )} />
+                    </div>
+                    <div id={"how-bt-wrapper"}>
+                        <Route render={({history}) => (
+                            <span
+                                id={"how-bt"}
+                                onClick={() => {
+                                    history.push("/how")
+                                }}>
+                            How To Play
+                        </span>
+                        )} />
+                    </div>
                 </div>
                 <span style={{fontWeight: "bold"}}><u>NB: Only landscape mode is supported</u></span>
             </div>
